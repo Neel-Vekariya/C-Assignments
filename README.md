f3fce1642a879d523bdfc4627d062fa605cc4dc9
Neel Vekariya <vekariyaneel007.in>
2025-10-16 21:06:06 +0530

    1-5 Stat Pattern

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# C-Assignments
-A complete collection of Star, Number, and Character Pattern Programs built using nested loops. Each pattern demonstrates logical thinking in programming. Perfect for learning pattern design in C for academic assignments.
+Hey there! 👋
+This repository contains my college assignment project a collection of Star, Number, and Character pattern programs.
+I created these patterns while practicing how loops and logic work in programming using C.
+Each pattern shows a creative way to use simple code to build shapes like triangles, pyramids, diamonds, and more.
+Through this project, I learned how a few lines of logic can turn into amazing visual patterns. ✨
+If you’re also learning programming or working on a similar assignment, this repo might be really helpful! 🚀
---
+++ Star Pattern/file_1.c
@@ -0,0 +1,85 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+
+int main(){
+    system("cls");
+    int i,j;
+    
+
+// Pattern 1: Right Triangle
+    for(i=1;i<6;i++){
+        for(j=1;j<=i;j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+
+// Pattern 2: Inverted Right Triangle
+    for(i=1;i<=5;i++){
+        for(j=5;j>=i;j--){
+            printf("*");
+        }
+        printf("\n");
+    }
+
+
+
+     
+// Pattern 3: Pyramid 
+for(i=0;i<5;i++){
+    for(j=1;j<=9;j++){
+        if(j>=5-i && j<=5+i){
+            printf("*");
+        }
+        else{
+            printf(" ");
+        } 
+    }
+    printf("\n");       
+    }
+
+
+
+// Pattern 4: Inverted Pyramid
+for(i=4;i>=0;i--){
+    for(j=1;j<=9;j++){
+        if(j>=5-i && j<=5+i){
+            printf("*");
+        }
+        else{
+            printf(" ");
+        } 
+    }
+    printf("\n");       
+    }
+
+
+
+
+// Pattern 5: Diamond 
+for(i=0;i<=4;i++){
+    for(j=1;j<=9;j++){
+        if(j>=5-i && j<=5+i){
+            printf("*");
+        }
+        else{
+            printf(" ");
+        } 
+    }
+    printf("\n");       
+    }
+for(i=3;i>=0;i--){
+    for(j=1;j<=9;j++){
+        if(j>=5-i && j<=5+i){
+            printf("*");
+        }
+        else{
+            printf(" ");
+        } 
+    }
+    printf("\n");       
+    }
+    return 0;
+}
